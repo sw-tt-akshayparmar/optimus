afb35bcb72ee1244805b645acad246072b76bc98
Akshay Parmar <akshay.parmar@softwebsolutions.com>
2025-07-11 20:02:11 +0530

    Fixes some issues in 6502

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,10 @@
-import type { Route } from "./+types/home";
 import "./home.css";
-import  _6502  from "~/components/_6502";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Optimus Gallery" },
-    { name: "description", content: "Welcome to Optimus Gallery Home!" },
-  ];
-}
+import H6502 from "~/components/H6502";
 
 export default function Home() {
-  return <_6502 />;
+  return (
+    <div className="home-container">
+      <H6502 />;
+    </div>
+  );
 }
