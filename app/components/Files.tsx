fa7e5ea62c8746935753c2fe0489535e405a4570
akshay <akshay>
2025-08-29 18:43:34 +0530

    Added workspace

--- app/components/Files.tsx
+++ app/components/Files.tsx
@@ -1,7 +1,8 @@
-export default function Fiels() {
+import "./files.css";
+export default function Files() {
   return (
     <div className="files-area">
-      <div className="files"></div>
+      <div className="files-list"></div>
     </div>
   );
 }
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,9 +1,14 @@
+import "./projects.css";
 export default function Projects() {
   return (
     <div className="project-list flex">
-      <input type="text" />
-      <button>Create Project</button>
-      <ul></ul>
+      <input
+        type="text"
+        className="project-input"
+        placeholder="New project name..."
+      />
+      <button className="project-create-btn">Create Project</button>
+      <ul className="project-items"></ul>
     </div>
   );
 }
