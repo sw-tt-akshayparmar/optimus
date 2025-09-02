d70d253f1923d0f1ca805011907e39bde80862ff
akshay <akshay>
2025-09-02 16:44:25 +0530

    a commit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { DataView } from "primereact/dataview";
+import { Skeleton } from "primereact/skeleton";
 import { Project as ProjectModel } from "../models/Project.model";
 import * as userService from "../services/user.service";
 import * as workspaceService from "../services/workspace.service";
@@ -15,6 +15,7 @@ export default function Projects() {
     async function fetchProjects() {
       setLoading(true);
       const data = await workspaceService.getAllProjects();
+      setLoading(false);
       setProjects(data);
     }
     fetchProjects();
@@ -29,17 +30,42 @@ export default function Projects() {
     });
     setNewProject("");
     setProjectDescription("");
+    setLoading(true);
     const saved: ProjectModel = await workspaceService.createProject(project);
+    setLoading(false);
     setProjects([saved, ...projects]);
   };
 
   const projectTemplate = (project: ProjectModel) => {
     return (
-      <div className="project-card card p-md m-sm flex flex-col gap-sm">
-        <h3 className="text-accent">{project.name}</h3>
-        <p className="text-secondary">{project.description}</p>
+      <div className="project">
+        <img src="https://cdn-icons-png.flaticon.com/256/4599/4599840.png" alt="Project" />
+        <div className="project-details p-md m-sm flex flex-col gap-sm">
+          <h3 className="text-accent">
+            <a href="javascript:void(0);" onClick={() => {}}>
+              {project.name}
+            </a>
+          </h3>
+          <p className="text-secondary">{project.description}</p>
+        </div>
       </div>
     );
+  };
+  const skeltonTemplate = (count = 3) => {
+    return "_"
+      .repeat(count)
+      .split("")
+      .map(() => {
+        return (
+          <div className="project">
+            <Skeleton shape="circle" size="50px" className="ml-4" />
+            <div className="project-details p-md m-sm flex flex-col gap-sm">
+              <Skeleton className="text-accent" />
+              <Skeleton className="text-secondary" />
+            </div>
+          </div>
+        );
+      });
   };
 
   return (
@@ -47,6 +73,7 @@ export default function Projects() {
       <div className="project-actions flex gap-sm">
         <input
           type="text"
+          id="projectName"
           className="project-input p-sm"
           placeholder="New project name..."
           value={newProject}
@@ -54,6 +81,7 @@ export default function Projects() {
         />
         <input
           type="text"
+          id="projectDescription"
           className="project-input"
           placeholder="Project Descrition (optional)"
           value={projectDescription}
@@ -67,15 +95,13 @@ export default function Projects() {
           Create Project
         </button>
       </div>
-
-      <DataView
-        value={projects}
-        itemTemplate={projectTemplate}
-        paginator
-        rows={5}
-        loading={loading}
-        className="project-items"
-      />
+      <div className="project-list">
+        {loading
+          ? skeltonTemplate()
+          : projects.map(project => {
+              return projectTemplate(project);
+            })}
+      </div>
     </div>
   );
 }
