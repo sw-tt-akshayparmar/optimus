a71f895bdcbf6650fda4fa17ba6bcb5a001916f2
Akshay Parmar <akshay.parmar@softwebsolutions.com>
2026-02-18 19:06:48 +0530

    - Last Message time added

--- src/app/features/chat/components/chat-container/chat-container.ts
+++ src/app/features/chat/components/chat-container/chat-container.ts
@@ -36,4 +36,23 @@ export class ChatContainer implements OnInit, OnDestroy {
     this.chatService.conversationId.set(c.id);
     this.router.navigate([c.id], { relativeTo: this.route });
   }
+  getTimeSinceLast(c: Conversation) {
+    const created = new Date(c?.messages?.[0]?.created_at!).getTime();
+    const now = Date.now();
+    const diff = now - created || 0;
+
+    const sec = Math.floor(diff / 1000);
+    const min = Math.floor(sec / 60);
+    const hr = Math.floor(min / 60);
+    const day = Math.floor(hr / 24);
+    const month = Math.floor(day / 30);
+    const year = Math.floor(day / 365);
+
+    if (year > 0) return `${year}y`;
+    if (month > 0) return `${month}m`;
+    if (day > 0) return `${day}d`;
+    if (hr > 0) return `${hr}h`;
+    if (min > 0) return `${min}min`;
+    return `${sec}s`;
+  }
 }
