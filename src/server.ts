c9ec16f644cb5e50f8acedd08e79635c706de6f4
Akshay Parmar <akshay.parmar@softwebsolutions.com>
2026-02-26 13:47:00 +0530

    - updates

--- src/server.ts
+++ src/server.ts
@@ -70,8 +70,8 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
  * The server listens on the port defined by the `PORT` environments variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  const port = Number(process.env['PORT']) || 4000;
+  app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
     }
